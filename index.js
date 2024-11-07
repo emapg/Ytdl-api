4a3b7f682811388d079aa0a3d43e6349c70539c1
emapg <emap04@proton.me>
2024-11-07 21:23:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.static('public'));
+
+// Serve the HTML page at the root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/download', async (req, res) => {
   const url = req.query.url;
